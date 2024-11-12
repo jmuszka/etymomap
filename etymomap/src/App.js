baa29a89b94e401ae99e293ffc959f770f397087
Josh Muszka <josh@jmuszka.com>
2024-11-11 23:01:43 -0500

    Display country borders and highlight

--- etymomap/src/App.js
+++ etymomap/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import GlobeBackground from './GlobeBackground.js'
 
--- etymomap/src/GlobeBackground.js
+++ etymomap/src/GlobeBackground.js
@@ -2,31 +2,40 @@ import React from 'react';
 import Globe from 'react-globe.gl';
 
 function GlobeBackground() {
-    const { useState, useEffect, useRef } = React;
+    const { useState, useEffect, useRef} = React;
+    const [countries, setCountries] = useState({ features: []});
+    const [hoverD, setHoverD] = useState();
     const globeEl = useRef();
 
     useEffect(() => {
+        // load data
+        fetch('countries.geojson').then(res => res.json()).then(setCountries);
+
         globeEl.current.controls().autoRotate = true;
         globeEl.current.controls().autoRotateSpeed = 0.8;
         //globeEl.current.controls().enableZoom = false;
 
         globeEl.current.pointOfView({ lat: 25, lng: 0, altitude: 1.2 }); // set globe angle, position, size
-      }, []);
+    }, []);
 
-    // Gen random data
-    const N = 300;
-    const gData = [...Array(N).keys()].map(() => ({
-        lat: (Math.random() - 0.5) * 180,
-        lng: (Math.random() - 0.5) * 360,
-        size: Math.random() / 3,
-        color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-    }));
 
-    return <Globe 
+    return <Globe
         ref={globeEl}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        pointAltitude="size"
-        pointColor="color"
+        //enablePointerInteraction={false}
+
+        globeImageUrl={'water.png'}
+        backgroundColor='#f6e6d0'
+        showAtmosphere={false}
+        lineHoverPrecision={0}
+        animateIn={false}
+
+        polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
+        //polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
+        polygonAltitude={0.01}
+        polygonCapColor={d => d === hoverD ? '#421854' : '#7d5125'}
+        polygonStrokeColor={() => '#111'}
+        onPolygonHover={setHoverD}
+        polygonsTransitionDuration={300}
     />;
     
 }
